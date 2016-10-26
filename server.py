abec744d477a7b955dbb66383b222e025c73c36b
ceyda aladağ <ceyda alada>
2016-10-26 21:21:59 +0300

    requirements are provided to make base page

--- server.py
+++ server.py
@@ -1,48 +1,38 @@
-import datetime
-import os
+from flask import Flask, render_template
 
-from flask import Flask
-from flask import render_template
+from datetime import datetime
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def layout_page():
-    now = datetime.datetime.now()
-    return render_template('layout.html', current_time=now.ctime())
-@app.route('/home')
 def home_page():
-    now = datetime.datetime.now()
-    return render_template('home.html', current_time=now.ctime())
+    now = datetime.now()
+    day = now.strftime('%A')
+    return render_template('home.html', day_name=day)
+
+
 @app.route('/profiles')
 def profiles():
-    now = datetime.datetime.now()
-    return render_template('profiles.html', current_time=now.ctime())
+    return render_template('profiles.html')
+
 @app.route('/settings')
 def settings():
-    now = datetime.datetime.now()
-    return render_template('settings.html', current_time=now.ctime())
+    return render_template('settings.html')
+
 @app.route('/notifications')
 def notifications():
-    now = datetime.datetime.now()
-    return render_template('notifications.html', current_time=now.ctime())
+    return render_template('notifications.html')
+
 @app.route('/favorites')
 def favorites():
-    now = datetime.datetime.now()
-    return render_template('favorites.html', current_time=now.ctime())
+    return render_template('favorites.html')
+
 @app.route('/followers')
 def followers():
-    now = datetime.datetime.now()
-    return render_template('followers.html', current_time=now.ctime())
-
+    return render_template('followers.html')
 
 
 if __name__ == '__main__':
-    VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
-    if VCAP_APP_PORT is not None:
-        port, debug = int(VCAP_APP_PORT), False
-    else:
-        port, debug = 5000, True
-    app.run(host='0.0.0.0', port=port, debug=debug)
+    app.run(host='0.0.0.0', port=5000, debug=True)
